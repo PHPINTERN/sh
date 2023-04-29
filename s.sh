d0078b2b30d2f3e6208e0b393de1aa4d3d16a84f
PHPINTERN <130788831+PHPINTERN@users.noreply.github.com>
2023-04-29 23:44:26 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y
 sudo apt-get install apache2 php wget unzip -y
 cd /var/www/
 sudo rm -r html
-sudo wget https://github.com/PHPINTERN/PHP/archive/refs/heads/FILE.zip 
+sudo wget https://github.com/PHPINTERN/PHP/archive/refs/heads/FILE.zip
 sudo unzip FILE.zip
 sudo mv PHP-FILE html
 sudo rm FILE.zip
