6f4aef4f6f91455b7d37ffcc8233133562f85b18
PHPINTERN <130788831+PHPINTERN@users.noreply.github.com>
2023-04-29 21:39:44 +0530

    Create s.sh

---
+++ s.sh
@@ -0,0 +1,9 @@
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install apache2 php wget unzip -y
+cd /var/www/html
+sudo wget https://github.com/PHPINTERN/PHP/archive/refs/heads/FILE.zip 
+unzip FILE.zip
+rm FILE.zip
