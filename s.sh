07e1342a7609f73f0e4b882c3988eaa0cc133c6d
PHPINTERN <130788831+PHPINTERN@users.noreply.github.com>
2023-04-30 14:38:41 +0530

    Update s.sh

--- s.sh
+++ s.sh
@@ -9,5 +9,6 @@ sudo wget https://github.com/PHPINTERN/PHP/archive/refs/heads/FILE.zip
 sudo unzip FILE.zip
 sudo mv PHP-FILE html
 sudo rm FILE.zip
+sudo service apache2 restart
 cd html
 sudo mysql -u root -p < Database.sql
