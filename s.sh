8f4d1c907e9fb2490268f3b9fded1489caf678dd
PHPINTERN <130788831+PHPINTERN@users.noreply.github.com>
2023-06-14 14:05:15 +0530

    s.sh

--- s.sh
+++ s.sh
@@ -6,5 +6,8 @@ sudo mv PHP-FILE html
 sudo rm FILE.zip
 sudo service apache2 restart
 cd html
+https://github.com/PHPINTERN/IMAGES/archive/refs/heads/main.zip
+sudo unzip FILE.zip
+sudo mv PHP-FILE IMAGE
 sudo mysql -u root -p < Database.sql
 
